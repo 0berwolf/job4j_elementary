7e69effb2fd60585ac7f9223fff04528c5f6d3b1
0berwolf <werewolf01051990@gmail.com>
2022-04-17 21:17:43 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/ru/job4j/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/ru/job4j/ru/job4j/condition/Point.java
@@ -41,10 +41,10 @@ public class Point {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
         double result1 = Point.distance(1, 3, 4, 2);
-        System.out.println("result (4, 2) to (1, 3) " + result1);
+        System.out.println("result (1, 3) to (4, 2) " + result1);
         double result2 = Point.distance(2, 3, 5, 5);
-        System.out.println("result (4, 2) to (1, 3) " + result2);
-        double result3 = Point.distance(8, 6, 5, 10);
-        System.out.println("result (4, 2) to (1, 3) " + result3);
+        System.out.println("result (2, 3) to (5, 5) " + result2);
+        double result3 = Point.distance(5, 6, 8, 10);
+        System.out.println("result (5, 6) to (8, 10) " + result3);
     }
 }
