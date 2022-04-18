c1440a6d3f1eaabc1f2e8d12c81803d763863c3e
0berwolf <werewolf01051990@gmail.com>
2022-04-18 21:14:15 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/ru/job4j/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/ru/job4j/ru/job4j/condition/Point.java
@@ -3,38 +3,14 @@ package ru.job4j.ru.job4j.ru.job4j.condition;
 public class Point {
 
     public static double distance(int x1, int y1, int x2, int y2) {
-        double d = Math.sqrt((x2 - x1) * (x2 - x1)  + (y2 - y1) * (y2 - y1));
-        return d;
-    }
-
-    public static double minus(int x2, int x1) {
-        int x = x2 - x1;
-        return x;
-    }
-
-    public static double minus1(int y2, int y1) {
-        int y = y2 - y1;
-        return y;
-    }
-
-    public static double pow(double x) {
-        double first = Math.pow(x, 2);
-        return first;
-    }
-
-    public static double pow1(double y) {
-        double second = Math.pow(y, 2);
-        return second;
-    }
-
-    public static double plus(double first, double second) {
-        double result = first + second;
-        return (result);
-    }
-
-    public static double sqrt(double result) {
-        double sqrt = Math.sqrt(result);
-        return sqrt;
+        double rsl = -1;
+        double x = x2 - x1;
+        double y = y2 - y1;
+        double one = Math.pow(x, 2);
+        double two = Math.pow(y, 2);
+        double three = one + two;
+        double four = Math.sqrt(three);
+        return four;
     }
 
     public static void main(String[] args) {
