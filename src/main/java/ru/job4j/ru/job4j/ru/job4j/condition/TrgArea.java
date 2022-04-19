e7df1aa6396d406445c9a233860291cc5b0a52d2
0berwolf <werewolf01051990@gmail.com>
2022-04-19 21:26:58 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/ru/job4j/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/ru/job4j/ru/job4j/condition/TrgArea.java
@@ -2,18 +2,13 @@ package ru.job4j.ru.job4j.ru.job4j.condition;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double rsl = -1;
         double p = (a + b + c) / 2;
-        double first = p - a;
-        double second = p - b;
-        double three = p - c;
-        double four = p * first * second * three;
-        double five = Math.sqrt(four);
-        return five;
+        double s = Math.sqrt(p * (p - a) * (p - b) * (p - b));
+        return s;
     }
 
     public static void main(String[] args) {
-        double rsl = TrgArea.area(2, 2, 2);
-        System.out.println("area (2, 2, 2) = " + rsl);
+        double p = TrgArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + p);
     }
 }
