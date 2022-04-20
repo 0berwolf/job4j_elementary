6734ba47b0c71e78a4ccec98ccfd6c4931b63a23
0berwolf <werewolf01051990@gmail.com>
2022-04-20 21:33:02 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Point.java
+++ src/main/java/ru/job4j/Point.java
@@ -1,11 +1,9 @@
-package ru.job4j.ru.job4j.ru.job4j.condition;
+package ru.job4j;
 
 public class Point {
 
     public static double distance(int x1, int y1, int x2, int y2) {
-        double x = x2 - x1;
-        double y = y2 - y1;
-        double d = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         return d;
     }
 
