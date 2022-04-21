8b7c2c6260ce61ddd5d0a5a2576a4cc077ff7a35
0berwolf <werewolf01051990@gmail.com>
2022-04-21 19:17:47 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/ru/job4j/converter/Converter.java
@@ -31,5 +31,26 @@ public class Converter {
         System.out.println("120 rubles are " + dollars + " dollars.");
         dollars = Converter.euroToDollar(12);
         System.out.println("12 euro is " + dollars + " dollars");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        float in1 = 120;
+        float expected1 = 2;
+        float out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("120 rubles are 2. Test result : " + passed1);
+        float in2 = 40;
+        float expected2 = in2 * 0.86f;
+        float out2 = Converter.dollarToEuro(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("40 dollars is 34.4. Test result : " + passed2);
+        float in3 = 12;
+        float expected3 = in3 * 1.17f;
+        float out3 = Converter.euroToDollar(in3);
+        boolean passed3 = expected3 == out3;
+        System.out.println("12 euro is 14.039999. Test result : " + passed3);
+
     }
 }
