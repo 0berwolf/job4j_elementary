40e4f056c1c2cc38191092c742c69892077d1172
0berwolf <werewolf01051990@gmail.com>
2022-04-21 23:00:41 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/calculator/Calculator1.java
+++ src/main/java/ru/job4j/calculator/Calculator1.java
@@ -1,15 +1,28 @@
 package ru.job4j.calculator;
 
-import ru.job4j.math.MathFunction;
+import static ru.job4j.math.MathFunction.*;
 
 public class Calculator1 {
 
     public static double sumAndMultiply(double first, double second) {
-        return MathFunction.sum(first, second)
-                + MathFunction.multiply(first, second);
+        return sum(first, second) + multiply(first, second);
+    }
+
+    public static double sum1(double first, double second) {
+        return mns(first, second) + delenie(first, second);
+    }
+
+    public static double sum2(double first, double second) {
+        return sum(first, second) + multiply(first, second) + mns(first, second) + delenie(first, second);
     }
 
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчёта равен: " + sum1(23, 36));
+        System.out.println("Результат расчёта равен: " + sum2(9, 18));
+
     }
+
 }
+
+
