5fa9783e7f28a60bf2b8bf5cfd2ea389d9910f68
0berwolf <werewolf01051990@gmail.com>
2022-04-22 15:36:30 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -6,15 +6,15 @@ import org.junit.Test;
 public class FitTest {
 
     @Test
-    public void whenManWeight182Then92() {
-        short in = 182;
+    public void whenMan180Then92() {
+        short in = 180;
         double expected = 92;
         double out = Fit.manWeight(in);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void whenWomanWeight170Then69() {
+    public void whenWoman170Then69() {
         short in = 170;
         double expected = 69;
         double out = Fit.womanWeight(in);
