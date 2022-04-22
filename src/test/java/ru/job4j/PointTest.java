c792e8c54cf61e3045efb6771ff29b76d95796af
0berwolf <werewolf01051990@gmail.com>
2022-04-22 20:37:36 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/test/java/ru/job4j/PointTest.java
@@ -0,0 +1,51 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when14to32then3() {
+        double expected = 3.16;
+        int x1 = 1;
+        int y1 = 3;
+        int x2 = 4;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when25to35then3() {
+        double expected = 3.61;
+        int x1 = 2;
+        int y1 = 3;
+        int x2 = 5;
+        int y2 = 5;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when58to610then5() {
+        double expected = 5;
+        int x1 = 5;
+        int y1 = 6;
+        int x2 = 8;
+        int y2 = 10;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
