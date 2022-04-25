02a175bee63e59e4fae8743569da9c2b96ea7a91
0berwolf <werewolf01051990@gmail.com>
2022-04-25 23:15:59 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+public class LogicNot {
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean notPositive(int num) {
+        return !isPositive(num);
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return notEven(num) && isPositive(num);
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return isEven(num) || notPositive(num);
+    }
+
+    public static void main(String[] args) {
+        boolean rsl = LogicNot.notEvenAndPositive(3);
+        System.out.println(rsl);
+        boolean rsl1 = LogicNot.evenOrNotPositive(2);
+        System.out.println(rsl1);
+    }
+}
