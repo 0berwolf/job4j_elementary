6490d1f6c6d1182a04c2b42f17df4f4a001ce15a
0berwolf <werewolf01051990@gmail.com>
2022-04-29 17:40:28 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int result = 1;
+        for (int calc = 1; calc <= n; calc++) {
+            result = result * calc;
+        }
+        return result;
+    }
+}
