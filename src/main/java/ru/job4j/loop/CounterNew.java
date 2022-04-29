bab160b148d9bfc211cc05bda22f98b67ed87ece
0berwolf <werewolf01051990@gmail.com>
2022-04-29 17:22:16 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/loop/CounterNew.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+public class CounterNew {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (sum = 0; start <= finish; start++) {
+            sum = sum + start;
+        }
+        return sum;
+    }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (sum = 0; start <= finish; start++) {
+            if (start % 2 == 0) {
+                sum = sum + start;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sum(2, 7));
+        System.out.println();
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
+        System.out.println(sumByEven(2, 7));
+    }
+}
