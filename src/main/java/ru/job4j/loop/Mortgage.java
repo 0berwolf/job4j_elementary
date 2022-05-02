c78340dce9ef0dc831892803fa2f4d2311cdadd5
0berwolf <werewolf01051990@gmail.com>
2022-05-02 22:40:16 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -5,8 +5,8 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            int credit = salary - (int) (amount * (percent / 100));
-            amount -= credit;
+            amount += amount * (percent / 100);
+            amount -= salary;
             year++;
     }
         return year;
