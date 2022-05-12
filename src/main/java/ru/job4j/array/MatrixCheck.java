afa500dad0d3bc9484ddc63dd7af496a3f161dbf
0berwolf <werewolf01051990@gmail.com>
2022-05-12 22:04:35 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,9 +4,12 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i <= row; i++) {
-            if (row == 0) {
-                result = false;
-                break;
+            for (int j = 0; j <= board[i].length; j++) {
+                if (board[i][j] == 'X') {
+                    char s = 'X';
+                    result = false;
+                    break;
+                }
             }
         }
         return result;
--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -24,7 +24,7 @@ public class MatrixCheckTest {
                 {' ', ' ', ' '},
                 {' ', ' ', ' '},
         };
-        int row = 0;
+        int row = 1;
         boolean result = MatrixCheck.monoHorizontal(input, row);
         Assert.assertFalse(result);
     }
