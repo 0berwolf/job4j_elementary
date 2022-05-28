b1aff503ed9c16414f9ef828a84bcac55a3c165c
0berwolf <werewolf01051990@gmail.com>
2022-05-28 18:08:22 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -7,28 +7,24 @@ public class MaxTest {
 
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int result = Max.max(1, 2);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax2To1Then2() {
-        int left = 2;
-        int right = 1;
-        int result = Max.max(left, right);
-        int expected = 2;
+    public void whenMax2To1To4Then4() {
+        Max max = new Max();
+        int result = max.max(2, 1, 4);
+        int expected = 4;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax2To2Then2() {
-        int left = 2;
-        int right = 2;
-        int result = Max.max(left, right);
-        int expected = 2;
+    public void whenMax1To5To3To2Then5() {
+        Max max = new Max();
+        int result = max.max(1, 5, 3, 2);
+        int expected = 5;
         Assert.assertEquals(result, expected);
     }
 }
